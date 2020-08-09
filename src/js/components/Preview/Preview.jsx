b2d67ee136f3a8705c7e27bed9110470bdb99acc
Quazaru <qilaur@mail.ru>
2020-08-09 15:11:38 +0300

    feat: add loading spinner

--- src/js/components/Preview/Preview.jsx
+++ src/js/components/Preview/Preview.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Preview.scss';
 import SwapiService from '../../modules/SwapiService';
 import renderInfo from '../../modules/renderInfo.jsx';
-
+import Spinner from '../Spinner/Spinner.jsx';
 
 class Preview extends React.Component {
   constructor(props) {
@@ -11,31 +11,39 @@ class Preview extends React.Component {
       tabName: this.props.tabName,
       id: this.props.id,
       currentData: false,
+      isLoading: true,
     };
   }
 
   updateData(service) {
     const { tabName, id } = this.props;
-    this.setState(() => ({tabName: tabName, id: id}));
+    this.setState(() => ({ tabName, id }));
     service.getElement(tabName, id)
-      .then((res) => this.setState({ currentData: { ...res } }));
+      .then((res) => this.setState({ currentData: { ...res }, isLoading: false }));
   }
 
   render() {
     if (this.props.tabName !== this.state.tabName || this.props.id !== this.state.id) {
+      this.setState({ isLoading: true });
       const service = new SwapiService();
       this.updateData(service);
     }
     const { tabName } = this.props;
+    const { isLoading } = this.state;
     const { infoList, img = '../../../assets/img/planet.svg' } = renderInfo(tabName, this.state.currentData);
     return (
       <div className="preview">
-        <div className="preview__img">
-          <img src={img} alt="planet-default" />
-        </div>
-        <div className="preview__body">
-          {infoList}
-        </div>
+        {isLoading && tabName !== 'main page' ? <Spinner />
+          : (
+            <div className="preview">
+              <div className="preview__img">
+                <img src={img} alt="planet-default" />
+              </div>
+              <div className="preview__body">
+                {infoList}
+              </div>
+            </div>
+          )}
       </div>
     );
   }
---
+++ src/js/components/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.scss';
+
+const Spinner = () => (
+  <div className="loadingio-spinner-eclipse-jba5f2z49ls">
+    <div className="ldio-jfo7b15ssh">
+      <div />
+    </div>
+  </div>
+);
+
+export default Spinner;
